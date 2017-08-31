0877b9aa9376d4b1106cc6205e97cfe50883fb24
Massimo La Morgia <massimo.lamorgia@uniroma1.it>
2017-08-31 17:16:35 +0200

    first ex

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,6 +12,8 @@
     
 import sys
 from time import time
+import sklearn as sk
+from sklearn.naive_bayes import GaussianNB
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -21,7 +23,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+t1 =time()
+print "fit time {}".format(round(t1-t0,3))
+t1=time()
+clf.predict(features_train)
+t2 = time()
+print "predict time {}".format(round(t2-t1,3))
 
+print "score :{}".format(clf.score(features_test,labels_test))
+t3=time()
+print "score time {}".format(round(t3-t2),3)
 
 
 #########################################################

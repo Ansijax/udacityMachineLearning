0aca7ca76ad7ae676cac7cae301170a37e405530
Massimo La Morgia <ansijax@gmail.com>
2017-09-01 16:34:52 +0200

    decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
 
 
 ### features_train and features_test are the features for the training
@@ -19,6 +20,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+
+clf = tree.DecisionTreeClassifier(min_samples_split=50)
+
+print "number of features {}".format(len(features_train[0]))
+
+t0= time()
+clf.fit(features_train,labels_train)
+t1 = time()
+
+print "fit time {}".format(round(t1-t0,3))
+accuracy =clf.score(features_test,labels_test)
+
+print "accuracy {}".format(accuracy)
 
 
 
